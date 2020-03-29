fccbc69b86e8c6f8a0a4a08fc7d8e705fc17d6ad
Elena Dinca <elenadinca25@gmail.com>
2020-03-29 22:58:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,16 @@
 def find_element_index(array, value_to_find)
-array.each { |f| require f }
+  array = [2,3,4,5]
+  value_to_find = 2
+  i = 0
+  while array[i] < array.length
+    if value_to_find == array[i]
+      puts i
+    else
+      puts nil
+      i += 1
+    end
+    i += 1
+  end
 
 
 def find_max_value(array)
